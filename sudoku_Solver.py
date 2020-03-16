00c11db887ea019dc5ce5a22b02fcdeb3ccd102c
Santiago Caro <santiagocaroduque@gmail.com>
2020-03-16 21:02:55 +0100

    Format Document

--- sudoku_Solver.py
+++ sudoku_Solver.py
@@ -1,13 +1,15 @@
-##-- Santiago Caro Duque
+# -- Santiago Caro Duque
+
 
 def read_game(file_name):
     f = open(file_name, 'r')
-    game = [ [ None for y in range( 9 ) ] for x in range( 9 ) ]
+    game = [[None for y in range(9)] for x in range(9)]
     for i in range(9):
         game[i] = f.readline().split()
     return game
 
-def get_posibilities(game,row,column):
+
+def get_posibilities(game, row, column):
     posibilities = []
     negative = []
     for i in range(9):
@@ -18,23 +20,25 @@ def get_posibilities(game,row,column):
     for j in range(9):
         if not game[j][column] in negative:
             negative.append(game[j][column])
-    quadrant_negative=get_quadrant_possibilities(game,row,column)
+    quadrant_negative = get_quadrant_possibilities(game, row, column)
 
     posibilities = list(set(posibilities)-set(negative))
     posibilities = list(set(posibilities) - set(quadrant_negative))
     return posibilities
 
-def get_quadrant_possibilities(game,row,column):
-    quadrant_row= int(int(row)/3)
-    quadrant_column= int(int(column)/3)
-    negative =[]
+
+def get_quadrant_possibilities(game, row, column):
+    quadrant_row = int(int(row)/3)
+    quadrant_column = int(int(column)/3)
+    negative = []
     rows = quadrant_row*3
-    colums = quadrant_column*3 
-    for i in range(rows,rows+3):
-        for j in range(colums,colums+3):
+    colums = quadrant_column*3
+    for i in range(rows, rows+3):
+        for j in range(colums, colums+3):
             if not game[i][j] in negative:
                 negative.append(game[i][j])
     return negative
+
 
 def done(game):
     for i in range(9):
@@ -50,8 +54,8 @@ def solve_game(game):
     else:
         for row in range(9):
             for column in range(9):
-                if game[row][column]=='0':
-                    posibilities = get_posibilities(game,row,column)
+                if game[row][column] == '0':
+                    posibilities = get_posibilities(game, row, column)
                     if len(posibilities) == 0:
                         return False
                     else:
@@ -59,7 +63,7 @@ def solve_game(game):
                             game[row][column] = k
                             if solve_game(game):
                                 return True
-                            game[row][column]='0'
+                            game[row][column] = '0'
                         return False
 
 
@@ -68,4 +72,4 @@ print(solve_game(game))
 
 
 for i in game:
-    print (*i,sep=" ")
+    print(*i, sep=" ")
